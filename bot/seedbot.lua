0ae10396b3836e21d8aaa5e91499af6a53206a08
MR_saf <jeiboor@users.noreply.github.com>
2016-09-11 05:38:58 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -258,7 +258,7 @@ function create_config( )
     "inAdmin"
 
     },
-    sudo_users = {187161700,160890821,207437078},--Sudo users
+    sudo_users = {187161700,160890821},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[ ]],
     help_text_realm = [[
